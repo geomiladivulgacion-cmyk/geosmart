53c93ab473da1c8ad65d6a32a96fb41dffe0ebde
Camila Isabel Fernández Riveras <camilaisabelfernandezriveras@Camilas-MacBook-Air.local>
2026-09-23 14:48:25 +0200

    Fix custom domain routing and About photo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://geosmart.es',
+  site: 'https://www.geosmart.es',
   base: '/',
   trailingSlash: 'always',
 });
